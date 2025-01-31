dd8f3b4089abdeb1c805c62a4cba65059b33546a
QihangITB <qihang.li.7e7@itb.cat>
2025-01-31 16:24:11 +0100

    add sql scripts

---
+++ PostgreSQL_Script/CreateTables.sql
@@ -0,0 +1,23 @@
+-- Crear la tabla de fragmentos
+CREATE TABLE fragment (
+    fragment_id SERIAL PRIMARY KEY,
+    order_number INT NOT NULL,
+    mensaje TEXT NOT NULL
+);
+
+-- Crear la tabla de colecciones
+CREATE TABLE collection (
+    collection_id SERIAL PRIMARY KEY,
+    fragment_list INT[] -- Lista de fragmentos como array de fragment_ids
+);
+
+-- Crear la tabla de usuarios
+CREATE TABLE player (
+    player_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    record_time TEXT,
+    collection_id INT,
+    FOREIGN KEY (collection_id) REFERENCES collection(collection_id)
+);
---
+++ PostgreSQL_Script/DropTables.sql
@@ -0,0 +1,4 @@
+-- Eliminar tablas
+DROP TABLE IF EXISTS collection;
+DROP TABLE IF EXISTS fragment;
+DROP TABLE IF EXISTS player;
